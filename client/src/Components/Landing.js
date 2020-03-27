90c61d1e026935bf43122fc3c09b9502512ff77d
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-03-27 23:57:30 +0530

    added Landing page

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Landing = () => {
-	return <div>test</div>;
+	return (
+		<section class='hero is-light is-fullheight'>
+			<div class='hero-body'>
+				<div class='container has-text-centered'>
+					<h1 class='title'>Welcome to Songify</h1>
+					<h2 class='subtitle'>Search for your favorite songs and artists</h2>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Landing;
